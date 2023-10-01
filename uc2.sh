615a190573531bb5d3c6968fababe9bbac3eacd2
maddaka abhi <maddakaabhilash@gmail.com>
2023-10-01 19:59:22 +0530

    coflict resoved

--- uc2.sh
+++ uc2.sh
@@ -1,10 +1,20 @@
 # !/bin/bash -x
+<<<<<<< HEAD
 ispresent=1
 empratehr=20
 check=$((RANDOM%2))
 if [ $ispresent -eq $check ];then
 emphr=8
 salary=$(($empratehr*$emphr))
+=======
+#calcualting daily employee wage
+ispresent=1
+wageperhr=20
+check=$((RANDOM%2))
+if [ $check -eq $ispresent ]; then
+emphr=8
+salary=$(($emphr*$wageperhr))
+>>>>>>> uc2
 echo $salary
 else
 salary=0
